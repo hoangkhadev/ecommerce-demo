6f39c9a97563e1aedd30575377ee95ac6042aa42
hoangkhadev <hoangkhadev@gmail.com>
2026-03-24 16:17:20 +0700

    feat(swagger): add Swagger/OpenAPI support

--- src/Ecommerce.Api/Program.cs
+++ src/Ecommerce.Api/Program.cs
@@ -16,12 +16,18 @@ namespace Ecommerce.Api
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // configure swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
